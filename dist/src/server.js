59ff79f5e1d1c7dc4b97eeba80048bc550acd585
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-09-17 22:54:29 -0300

    add: criando rotas da API de jogos e anuncios

--- dist/src/server.js
+++ dist/src/server.js
@@ -5,8 +5,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-app.get('/', (req, res) => {
-    return res.status(200).json({ message: 'hey' });
+app.get('/games', (req, res) => {
+    return res.status(200).json([]);
+});
+app.post('/ads', (req, res) => {
+    return res.status(201).json([]);
+});
+app.get('/games/:id/ads', (req, res) => {
+    const { id } = req.params;
+    return res.status(200).json([
+        { id: 1, name: 'anuncio 1' }
+    ]);
+});
+app.get('/ads/:id/discord', (req, res) => {
+    return res.status(200).json([]);
 });
 const PORT = 3001;
 app.listen(PORT, () => console.log(`Listening at port ${PORT}`));
--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,25 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  return res.status(200).json({ message: 'hey' });
+app.get('/games', (req: Request, res: Response) => {
+  return res.status(200).json([]);
+});
+
+app.post('/ads', (req: Request, res: Response) => {
+  return res.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  return res.status(200).json([
+    { id: 1, name: 'anuncio 1' }
+  ]);
+});
+
+app.get('/ads/:id/discord', (req: Request, res: Response) => {
+  return res.status(200).json([]);
+
 });
 
 const PORT = 3001;
